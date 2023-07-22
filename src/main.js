6062e5e87499335cde584cfdcbc1e0034c29e4fe
RoyhanRPL2 <royhanbumi227@gmail.com>
2023-07-22 10:08:42 +0700

    feat:[RB] add wishlist button

--- src/main.js
+++ src/main.js
@@ -11,11 +11,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faStar, faUserSecret, faArrowRight, faEye, faEyeSlash, faClock, faChevronDown, faCalendarDays, faCircleExclamation, faMoneyBillWave, faMoneyBill } from '@fortawesome/free-solid-svg-icons'
+import { faStar, faUserSecret, faArrowRight, faEye, faEyeSlash, faClock, faChevronDown, faCalendarDays, faCircleExclamation, faMoneyBillWave, faMoneyBill, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faUserSecret, faLocationDot, faStar, faArrowRight, faEye, faEyeSlash, faClock, faChevronDown, faCalendarDays, faCircleExclamation, faMoneyBillWave, faMoneyBill)
+library.add(faUserSecret, faLocationDot, faStar, faArrowRight, faEye, faEyeSlash, faClock, faChevronDown, faCalendarDays, faCircleExclamation, faMoneyBillWave, faMoneyBill, faPlus)
 
 const app = createApp(App)
 
