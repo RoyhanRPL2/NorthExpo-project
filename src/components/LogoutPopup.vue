a96bfb15ec54e401c571c3eae3ae0ecbca07c09f
RoyhanRPL2 <royhanbumi227@gmail.com>
2023-08-05 23:46:01 +0700

    perf:[RB] add pop up for logout confirmation

--- src/components/LogoutPopup.vue
+++ src/components/LogoutPopup.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="logout-popup" v-if="visible">
-        <div class="popup-content">
-            <h3>Logout Confirmation</h3>
-            <p>Are you sure you want to logout?</p>
-            <div class="popup-buttons">
-                <button @click="cancelLogout">Cancel</button>
-                <button @click="confirmLogout">Logout</button>
-            </div>
-        </div>
+  <div class="logout-popup" v-if="visible">
+    <div class="popup-content">
+      <font-awesome-icon class="logout-icon" icon="fa-solid fa-arrow-right-from-bracket" />
+      <p>Apakah anda yakin ingin keluar?</p>
+      <div class="popup-buttons">
+        <button @click="cancelLogout">Cancel</button>
+        <button @click="confirmLogout">Logout</button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
@@ -34,23 +34,71 @@ function confirmLogout() {
   position: fixed;
   top: 0;
   left: 0;
+  z-index: 1000;
   width: 100%;
-  height: 100%;
+  height: 100vh;
   background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
 }
 
 .popup-content {
-  background-color: white;
-  padding: 20px;
-  border-radius: 5px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: var(--color-theme-950);
+  padding: 30px;
+  border-radius: 10px;
+  width: 400px;
+  text-align: center;
+}
+
+.logout-icon {
+  color: #fff;
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
+}
+
+.popup-content h3 {
+  color: #fff;
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+  font-weight: 600;
+}
+
+.popup-content p {
+  color: #fff;
+  font-size: 1rem;
+  margin-bottom: 1rem;
 }
 
 .popup-buttons {
-  margin-top: 10px;
-  text-align: right;
+  display: flex;
+  justify-content: center;
+}
+
+.popup-buttons button {
+  padding: 0.5rem 1rem;
+  border-radius: 5px;
+  border: none;
+  background-color: var(--color-primary-500);
+  color: #fff;
+  font-size: 1rem;
+  font-weight: 700;
+  cursor: pointer;
+  margin: 0 0.5rem;
+}
+
+.popup-buttons button:first-child {
+  background-color: var(--color-theme-950);
+}
+
+.popup-buttons button:hover {
+  opacity: 0.8;
+}
+
+@media screen and (max-width: 768px) {
+  .popup-content {
+    width: 300px;
+  }
 }
 </style>
