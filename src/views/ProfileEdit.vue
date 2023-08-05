30c8931bae7fb0420931e01f8439d30ec6746d0b
Mavis Daffa Wisanggeni <mavisdaffawisanggeni22@gmail.com>
2023-08-05 07:52:09 +0700

    fix: [MW] edit profile

--- src/views/ProfileEdit.vue
+++ src/views/ProfileEdit.vue
@@ -1,26 +1,36 @@
 <template>
-<div>
-    <h2>Edit Profile</h2>
-    <form @submit.prevent="saveProfile">
-        <div>
-            <label for="avatar">Photo:</label>
-            <img :src="'https://admin.api.northexpokudus.com/assets/img/avatar/' + user.user.avatar" alt="" class="user-avatar">
-            <input type="file" id="avatar" @change="handlePhotoUpload" accept="image/*">
+    <div class="update-profile-container">
+        <div class="container">
+            <h2>Ubah Profil</h2>
+            <form @submit.prevent="saveProfile">
+                <div>
+                    <label for="avatar">Foto Kamu :</label>
+                    <img :src="'https://admin.api.northexpokudus.com/assets/img/avatar/' + user.user.avatar" alt=""
+                        class="user-avatar">
+                    <input type="file" id="avatar" @change="handlePhotoUpload" accept="image/*">
+                </div>
+                <div>
+                    <div>
+                        <label for="name">Nama :</label>
+                        <input type="text" id="name" v-model="user.user.name">
+                    </div>
+                    <div>
+                        <label>Email :</label>
+                        <p>{{ user.user.email }}</p>
+                    </div>
+                    <div class="action">
+                        <button type="submit">Simpan</button>
+                        <router-link to="/profile">
+                            <button class="kembali" type="button">Kembali</button>
+                        </router-link>
+                    </div>
+                </div>
+            </form>
         </div>
-        <div>
-            <label for="name">Name:</label>
-            <input type="text" id="name" v-model="user.user.name">
-        </div>
-        <div>
-            <label>Email:</label>
-            <p>{{ user.user.email }}</p>
-        </div>
-        <button type="submit">Simpan</button>
-        <router-link to="/profile">
-            <button class="kembali" type="button">Kembali</button>
-        </router-link>
-    </form>
-</div>
+
+        <div class="circle-bg"></div>
+        <div class="circle-bg"></div>
+    </div>
 </template>
 
 <script>
@@ -55,11 +65,11 @@ export default {
             }
 
             axios.post(`https://admin.api.northexpokudus.com/api/auth/user/${id}`, formData, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data',
-                    },
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
 
-                })
+            })
                 .then(response => {
                     console.log('Profile successfully saved:', response.data);
                     // Simpan data pengguna yang diperbarui ke local storage
@@ -79,10 +89,32 @@ export default {
 
 <!-- style kode diatas -->
 <style scoped>
+.update-profile-container {
+    width: 100%;
+    height: 100vh;
+    padding: 50px 100px;
+}
+
+.update-profile-container .container {
+    background-color: rgba(255, 255, 255, 0.1);
+    width: 100%;
+    height: 100%;
+    padding: 50px;
+    border-radius: 30px;
+    display: flex;
+    overflow: hidden;
+    flex-direction: column;
+    align-items: center;
+    backdrop-filter: blur(120px);
+    border: 1px solid var(--color-theme-100);
+}
+
 h2 {
+    font-size: 34px;
+    font-weight: 700;
+    line-height: 1.3;
     color: var(--color-theme-950);
-    padding: 100px 0 0 100px;
-    font-weight: 600;
+    margin-bottom: 100px;
 }
 
 .user-avatar {
@@ -95,9 +127,8 @@ h2 {
 
 form {
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     gap: 1rem;
-    padding: 50px 100px 100px 100px;
     color: var(--color-theme-950);
 }
 
@@ -109,13 +140,20 @@ form div {
 
 form div label {
     font-weight: bold;
+    font-size: 16px;
+    margin-bottom: 5px;
 }
 
 form div input {
-    padding: 0.5rem;
-    border-radius: 0.5rem;
+    padding: 1rem;
     border: 1px solid var(--color-theme-950);
     width: fit-content;
+    margin-bottom: 10px;
+    background-color: transparent;
+}
+
+form div input#name {
+    border-radius: 0.5rem;
 }
 
 form div input:focus {
@@ -126,10 +164,18 @@ form div input:focus {
 form div input[type="file"] {
     padding: 0;
     border: none;
+    margin-top: 20px;
 }
 
 form div input[type="file"]:focus {
     outline: none;
+}
+
+form .action {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-top: 20px;
 }
 
 form button {
@@ -151,8 +197,66 @@ form button:hover {
     background-color: var(--color-primary-600);
 }
 
-form .kemmbali {
+form .kembali {
     background-color: transparent;
-    border: 1px solid var(--color-theme-950);
+    border: 1px solid var(--color-primary-600);
+    color: var(--color-primary-600);
+    transition: .3s;
+}
+
+form .kembali:hover {
+    background-color: var(--color-primary-600);
+    color: var(--color-theme-50);
+}
+
+.circle-bg {
+    position: absolute;
+    top: 100px;
+    right: 100px;
+    width: 250px;
+    height: 250px;
+    background-color: var(--color-primary-400);
+    border-radius: 50%;
+    z-index: -1;
+    animation: circleBg1 15s ease-in-out infinite;
+}
+
+.circle-bg:nth-child(2) {
+    bottom: 300px;
+    left: 300px;
+    width: 200px;
+    height: 200px;
+    background-color: rgb(255, 0, 208);
+    border-radius: 50%;
+    z-index: -1;
+    animation: circleBg2 13s ease-in-out infinite;
+}
+
+@keyframes circleBg1 {
+    0% {
+        transform: translateX(0px);
+    }
+
+    50% {
+        transform: translateX(-1000px);
+    }
+
+    100% {
+        transform: translateX(0px);
+    }
+}
+
+@keyframes circleBg2 {
+    0% {
+        transform: translateX(0px);
+    }
+
+    50% {
+        transform: translateY(400px);
+    }
+
+    100% {
+        transform: translateX(0px);
+    }
 }
 </style>
